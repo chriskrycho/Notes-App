aebb5f221a928b37334fe1f36017302a05475d90
Chris Krycho <chris@chriskrycho.com>
2016-07-02 18:00:15 -0400

    Initial Commit

---
+++ Notes/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Notes
+//
+//  Created by Christopher Krycho on 7/2/16.
+//  Copyright © 2016 Metacognitive Software. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
